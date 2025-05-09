b267d1e9f790359dfc51306085ecb1fe478d6e17
bsh1706 <84366169+bsh1706@users.noreply.github.com>
2025-05-09 14:16:40 +0900

    Merge

--- frontend/src/routes/Signup.js
+++ frontend/src/routes/Signup.js
@@ -11,7 +11,7 @@ function Signup() {
 
         {/* 이름 아이디 비밀번호 메인 콘텐츠 영역 */}
         <div className="auth-content">
-          <FormGroup id="name" label="이름" placeholder="이름을 입력하세요" />
+          <FormGroup id="username" label="이름" placeholder="이름을 입력하세요" />
           <FormGroup id="email" label="이메일" type="email" placeholder="이메일을 입력하세요" />
           <FormGroup id="password" label="비밀번호" type="password" placeholder="비밀번호를 입력하세요" />
           <FormGroup id="confirm-password" label="비밀번호 확인" type="password"placeholder="비밀번호를 다시 입력하세요"/>
