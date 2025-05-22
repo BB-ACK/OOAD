087da5eff6277f939d03bb13189fd6ed8abeed03
bsh1706 <84366169+bsh1706@users.noreply.github.com>
2025-05-23 03:06:48 +0900

    course 경로 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./routes/Login"
 import Signup from "./routes/Signup"
 import Home from "./routes/Home"
-import { isAuthenticated } from "./utils/auth"
+import Course from "./routes/Course"
 import "./styles/common.css"
 
 function App() {
@@ -12,11 +12,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/course" element={<Course />} />
         {/* 루트 경로는 인증 상태에 따라 홈 또는 로그인으로 리다이렉트 */}
-        <Route
-          path="/"
-          element={<Navigate to="/login" replace />}
-        />
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
