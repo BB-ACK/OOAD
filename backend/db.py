bf1b7cfb46ae0d6fe11928fc0cfcb596503f4eaf
BB-ACK <tbvj1952@naver.com>
2025-07-20 22:22:42 +0900

    DB 연결 테스트

--- backend/db.py
+++ backend/db.py
@@ -6,12 +6,16 @@ import os
 
 # # .env파일에서 설정 불러오기 by os
 # load_dotenv()
-
-client = MongoClient(os.getenv('MONGODB_URI'))
+try:
+    client = MongoClient(os.getenv('MONGODB_URI'))
+    print("✅ MongoDB 연결 성공:", client.server_info())
+except Exception as e:
+    print("❌ MongoDB 연결 실패:", e)
 db = client["feelGood"] # feelGood 이라는 이름의 데이터베이스 가져오기(없으면 생성)
 users_col = db["users"] # users 이라는 이름의 컬렉션(=테이블) 가져오기(없으면 생성)
 places_col = db["places"]
 courses_col = db["courses"]
+
 
 # seed 데이터 등록하는 함수
 def insert_seed_data():
